89b194fb63f7b93736ab753447202f415d6f37cc
JOSHIM001 <148627863+JOSHIM001@users.noreply.github.com>
2026-08-28 12:17:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (29.74%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        freq={}
+        words=s.split(" ")
+        if len(words)!=len(pattern):
+            return False
+        for i in range(len(pattern)):
+            p=pattern[i]
+            word=words[i]
+            if p in freq:
+                if freq[p]!=word:
+                    return False
+            else:
+                if word in freq.values():
+                    return False
+            freq[p]=word
+        return True
