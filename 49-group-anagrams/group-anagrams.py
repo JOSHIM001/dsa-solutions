fa8c66ae850140338bb608ecf512e1de6e83f2ef
JOSHIM001 <148627863+JOSHIM001@users.noreply.github.com>
2026-09-09 14:28:20 +0530

    Time: 18 ms (21.73%) | Memory: 23.9 MB (26.63%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        if len(strs)==0:
+            return []
+        ans={}
+        for s in strs:
+            count=[0]*26
+            for c in s:
+                count[ord(c)-ord('a')]+=1
+            key=tuple(count)
+            if key not in ans:
+                ans[key]=[]
+            ans[key].append(s)
+        return list(ans.values())
